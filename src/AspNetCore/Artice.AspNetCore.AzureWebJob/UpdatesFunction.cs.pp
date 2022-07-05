5d0e94fd5d1f89c239d6506f0aa329a7a6910412
Fatkullin Timur <fatkullint@gmail.com>
2022-07-05 13:57:09 +0200

    +Update function template

--- src/AspNetCore/Artice.AspNetCore.AzureWebJob/UpdatesFunction.cs.pp
+++ src/AspNetCore/Artice.AspNetCore.AzureWebJob/UpdatesFunction.cs.pp
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 
-namespace Artice.AzureWebFunctions
+namespace $rootnamespace$
 {
     public class UpdatesFunction : UpdatesFunctionBase
     {
