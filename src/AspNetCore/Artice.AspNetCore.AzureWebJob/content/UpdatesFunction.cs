b1c9501dfd5f0d0a2cde8dbf978db407fc7cdc7f
Fatkullin Timur <fatkullint@gmail.com>
2022-07-05 16:33:41 +0200

    fixed content structure

---
+++ src/AspNetCore/Artice.AspNetCore.AzureWebJob/content/UpdatesFunction.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Threading.Tasks;
+using Artice.AspNetCore.AzureWebJob;
+using Artice.Core.AspNetCore.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+
+namespace Artice.AspNetCore.AzureWebJob
+{
+    public class UpdatesFunction : UpdatesFunctionBase
+    {
+        public UpdatesFunction(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+
+        [FunctionName("Updates")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "updates/{provider}")] HttpRequest req,
+            string provider)
+        {
+            var webHookResult = await base.ProcessUpdate(req, provider);
+            return GetActionResult(webHookResult);
+        }
+
+        private static IActionResult GetActionResult(WebhookResponse response)
+        {
+            return new ObjectResult(response.Body)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
