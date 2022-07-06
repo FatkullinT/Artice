6014ed5f308be45688b3d2990fca87c95ec860dc
Fatkullin Timur <fatkullint@gmail.com>
2022-07-06 17:40:24 +0200

    Changed file location

--- src/AspNetCore/Artice.AspNetCore.AzureWebJob/UpdatesFunctionBase.cs
+++ src/AspNetCore/Artice.AspNetCore.AzureWebJob/UpdatesFunctionBase.cs
@@ -10,7 +10,7 @@ using Artice.Core.Logger;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Artice.AspNetCore.AzureWebJob.Functions.Http
+namespace Artice.AspNetCore.AzureWebJob
 {
     public class UpdatesFunctionBase
     {
